a5ea07130aa25ba50b18316750da10824cfca4bb
AyoubNL <gmilio@live.nl>
2025-01-17 15:18:10 +0100

    First commit

---
+++ backend-it-helpdesk/src/main/java/nl/novi/backend_it_helpdesk/BackendItHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.backend_it_helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendItHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendItHelpdeskApplication.class, args);
+	}
+
+}
---
+++ backend-it-helpdesk/src/test/java/nl/novi/backend_it_helpdesk/BackendItHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.backend_it_helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendItHelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
