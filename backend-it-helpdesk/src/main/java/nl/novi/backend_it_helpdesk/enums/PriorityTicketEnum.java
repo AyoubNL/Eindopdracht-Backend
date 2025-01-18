176a3e6a99f61987e22ff8b870ad5a885a663fec
AyoubNL <gmilio@live.nl>
2025-01-18 22:54:36 +0100

    created priorityticketenum

---
+++ backend-it-helpdesk/src/main/java/nl/novi/backend_it_helpdesk/enums/PriorityTicketEnum.java
@@ -0,0 +1,8 @@
+package nl.novi.backend_it_helpdesk.enums;
+
+public enum PriorityTicketEnum {
+    P1_Organization,
+    P2_Departement,
+    P3_Team,
+    P4_Individual;
+}
--- backend-it-helpdesk/src/main/java/nl/novi/backend_it_helpdesk/models/Ticket.java
+++ backend-it-helpdesk/src/main/java/nl/novi/backend_it_helpdesk/models/Ticket.java
@@ -1,4 +1,20 @@
 package nl.novi.backend_it_helpdesk.models;
 
+import jakarta.persistence.*;
+import nl.novi.backend_it_helpdesk.enums.PriorityTicketEnum;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tickets")
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Date createdAt;
+    private PriorityTicketEnum priority;
+
 }
