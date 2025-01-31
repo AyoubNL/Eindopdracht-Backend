acb480179fdcab38bce99b6cfc6a3de9650e5651
AyoubNL <gmilio@live.nl>
2025-01-31 13:39:26 +0100

    created transferToDto

--- backend-it-helpdesk/src/main/java/nl/novi/backend_it_helpdesk/mappers/TicketMapper.java
+++ backend-it-helpdesk/src/main/java/nl/novi/backend_it_helpdesk/mappers/TicketMapper.java
@@ -5,7 +5,20 @@ import nl.novi.backend_it_helpdesk.models.Ticket;
 
 public class TicketMapper {
 
+    public static Ticket transferToDto(TicketOutputDto ticketOutputDto) {
 
+        Ticket ticket = new Ticket();
+
+        ticket.setId(ticketOutputDto.getId());
+        ticket.setCreatedAt(ticketOutputDto.getCreatedAt());
+        ticket.setCreatedBy(ticketOutputDto.getCreatedBy());
+        ticket.setPriority(ticketOutputDto.getPriority());
+        ticket.setCategory(ticketOutputDto.getCategory());
+        ticket.setDetail(ticketOutputDto.getDetail());
+        ticket.setFix(ticketOutputDto.getFix());
+        ticket.setScreenshots(ticketOutputDto.getScreenshots());
+
+    }
 
 
 
