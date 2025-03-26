aac840d0753181e037c6a075359b2867f67b7b43
AyoubNL <gmilio@live.nl>
2025-03-26 06:02:34 +0100

    mockticket error

--- backend-it-helpdesk/src/main/resources/data.sql
+++ backend-it-helpdesk/src/main/resources/data.sql
@@ -2,3 +2,12 @@ INSERT INTO users (username, password, role, email)
 VALUES ('Test01', '$2a$10$oh2jnssxP9pT/TcHPfhXb.au5/Fn1.7/AO.A1dRKA1jhunmuybd0.', 'MANAGER', 'test01@novi.nl'),
        ('Test02', '$2a$10$.k.Ug5Pf7CGRf/QIw5zuy.BYCH17d5R.IlxHxS1r5SeZXgD6ptKRW', 'AGENT', 'test02@novi.nl'),
        ('Test03', '$2a$10$wMMChXMeYRqPSwSP/4Nns.CrFArfWhaBfswig.ljtEjbSvnd45gn6', 'CLIENT', 'test03@novi.nl');
+
+INSERT INTO tickets(CATEGORY_ID, CLOSED_AT, CREATED_AT, DETAIL_ID, FIX_ID, PRIORITY, USER_ID)
+VALUES (1,'',localtime, 1,1);
+
+INSERT INTO categories(id, category_name, sub_category_name)
+VALUES (1, 'Hardware', 'Mouse');
+
+INSERT INTO details(id, description, title, type)
+VALUES (1, 'Mijn muis kan ik niet verbinden met de laptop (Dell)', 'Geen verbinding', 'MALFUNCTION')
--- backend-it-helpdesk/src/test/java/nl/novi/backend_it_helpdesk/services/TicketServiceTest.java
+++ backend-it-helpdesk/src/test/java/nl/novi/backend_it_helpdesk/services/TicketServiceTest.java
@@ -1,16 +1,43 @@
 package nl.novi.backend_it_helpdesk.services;
 
+import nl.novi.backend_it_helpdesk.models.Ticket;
+import nl.novi.backend_it_helpdesk.repositories.TicketRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TicketServiceTest {
+
+    @Mock
+    private TicketRepository ticketRepository;
+
+    @InjectMocks
+    private TicketService ticketService;
+
+    private List<Ticket> mockTickets;
 
     @BeforeEach
     void setUp() {
+
+        mockTickets = Arrays.asList((new Ticket(1L, "2025-03-26 04:27:13.287764", "P1_ORGANIZATION", "2025-03-26 05:27:13.287764", 1, 1, 1, 1, "Test01")),
+                (new Ticket(2L, "2025-03-26 05:27:13.287764", "P2_DEPARTEMENT", "2025-03-26 06:27:13.287764", 2, 2, 2, 2, "Test02")),
+                (new Ticket(3L, "2025-03-26 06:27:13.287764", "P3_TEAM", "2025-03-26 07:27:13.287764", 3, 3, 3, 3, "Test03"))
+        );
     }
 
     @Test
-    void getTicketById() {
+    void testGetTicketById() {
+        when(ticketRepository.findById(2L)).thenReturn(mockTickets.subList(0, 2));
+
+
     }
 }
